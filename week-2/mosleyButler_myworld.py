d20e2c369e1e05e37e0da00094b1140c3f63b5a0
TiaNiecia Mosley Butler <Butlertm1165@gmail.com>
2023-06-03 15:45:30 -0600

    added week 2 execrise 2.3

---
+++ week-2/mosleyButler_myworld.py
@@ -0,0 +1,9 @@
+"""
+    Title: output_ex2.py
+    Author: Professor Krasso
+    Date: 11 August 2022
+    Description: Example file for outputting values.
+
+"""
+
+print("You are now in Mosley Butler's world!")
